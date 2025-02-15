b3f8e21ab7d669659502803fe696d6557238b5e1
Van Chi Hieu <vanchihieu3@gmail.com>
2025-02-15 20:21:44 +0700

    Sử dụng MongoDB với NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://vanchihieu3:f6EH0nOF0bGZiQ7K@cluster0.sb1yd.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
