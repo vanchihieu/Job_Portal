a1799c7baf4d3a21e58d4bfd82587cbdf7d1d955
Van Chi Hieu <vanchihieu3@gmail.com>
2025-02-15 22:33:34 +0700

    Create Scheme (model)

---
+++ src/users/schemas/user.shema.ts
@@ -0,0 +1,70 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true }) //Prop thuộc tính
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  name: string;
+
+  // @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Role.name })
+  // role: mongoose.Schema.Types.ObjectId;
+
+  @Prop()
+  refreshToken: string;
+
+  @Prop()
+  age: string;
+
+  @Prop({ type: Object })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
+
+  @Prop()
+  gender: string;
+
+  @Prop()
+  address: string;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updateAt: Date;
+
+  // 2 trường cuối là dành cho soft delete
+  @Prop()
+  isDeleted: boolean;
+
+  @Prop()
+  deleteAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
