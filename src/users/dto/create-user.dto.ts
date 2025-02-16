81c9490546dedaf23327e417fd9c3d40e1666d89
Van Chi Hieu <vanchihieu3@gmail.com>
2025-02-16 12:29:30 +0700

    DTO - create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,93 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsMongoId,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
+
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
+
+//class = object
+export class CreateUserDto {
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password không được để trống' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Name không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Age không được để trống' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Gender không được để trống' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'Address không được để trống' })
+  address: string;
+
+  @IsNotEmpty({ message: 'Role không được để trống' })
+  @IsMongoId({ message: 'Role có định dạng là mongo Id' })
+  role: mongoose.Schema.Types.ObjectId;
+
+  //Validate 1 biến object
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company!: Company;
+}
+
+export class RegisterUserDto {
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password không được để trống' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Name không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Age không được để trống' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Gender không được để trống' })
+  gender: number;
+
+  @IsNotEmpty({ message: 'Address không được để trống' })
+  address: number;
+}
+
+export class UserLoginDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: 'chungphong', description: 'username' })
+  readonly username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '123456',
+    description: 'password',
+  })
+  readonly password: string;
+}
