c5cc247bb0dd368cc88ae6e08e67e8b122f45b1f
Van Chi Hieu <vanchihieu3@gmail.com>
2025-02-18 19:52:28 +0700

    Passing Requser

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -10,14 +10,16 @@ import {
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { IUser } from 'src/users/users.interface';
+import { User } from 'src/decorator/customize';
 
 @Controller('companies')
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto) {
-    return this.companiesService.create(createCompanyDto);
+  create(@Body() createCompanyDto: CreateCompanyDto, @User() user: IUser) {
+    return this.companiesService.create(createCompanyDto, user); //rồi truyền sang phần service
   }
 
   @Get()
