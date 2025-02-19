44d527dbc5dc89717449882b883004d10af222c1
Van Chi Hieu <vanchihieu3@gmail.com>
2025-02-19 19:45:07 +0700

    update user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,74 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
+
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
 
 //class = object
 export class CreateUserDto {
-  @IsEmail({}, { message: 'Email khoong dung dinh dang' })
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
   @IsNotEmpty({
-    message: 'Email khong duoc de trong',
+    message: 'Email không được để trống',
   })
   email: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Password không được để trống' })
   password: string;
 
+  @IsNotEmpty({ message: 'Name không được để trống' })
   name: string;
+
+  @IsNotEmpty({ message: 'Age không được để trống' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Gender không được để trống' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'Address không được để trống' })
   address: string;
+
+  @IsNotEmpty({ message: 'Role không được để trống' })
+  role: string;
+
+  //Validate 1 biến object
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company!: Company;
+}
+
+export class RegisterUserDto {
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
+  email: string;
+
+  @IsNotEmpty({ message: 'Password không được để trống' })
+  password: string;
+
+  @IsNotEmpty({ message: 'Name không được để trống' })
+  name: string;
+
+  @IsNotEmpty({ message: 'Age không được để trống' })
+  age: number;
+
+  @IsNotEmpty({ message: 'Gender không được để trống' })
+  gender: number;
+
+  @IsNotEmpty({ message: 'Address không được để trống' })
+  address: number;
 }
